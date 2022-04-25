5bf6b7da0402f684cd2889017c8b2f22c88bf4c1
iprankurpandey <pprankur@gmail.com>
2022-04-25 18:12:25 +0530

    implemented RWD

--- exc.js
+++ exc.js
@@ -1,23 +1,13 @@
-const input = [
-  { name: "Sreekanth", age: 30, gender: "M" },
-  { name: "Sai", age: 30, gender: "M" },
-  { name: "Shaik", age: 30, gender: "F" },
-  { name: "Sreekanth", age: 24, gender: "M" },
-  { name: "Sai", age: 21, gender: "F" },
-];
+const menuIcon = document.querySelector(".nav-menu-icon");
+const aside = document.querySelector(".aside");
 
-const customSort = (person1, person2) => {
-  if (person1.name < person2.name) {
-    return -1;
-  } else if (person2.name > person1.name) {
-    return 1;
+function showAside() {
+  if (aside.style.display === "none") {
+    aside.style.display = "block";
   } else {
-    if (person1.age < person2.age) {
-      return -1;
-    } else {
-      return 1;
-    }
+    aside.style.display = "none";
+    console.log(aside);
   }
-  return 0;
-};
-console.log(input.sort(customSort));
+}
+
+menuIcon.addEventListener("click", showAside);
