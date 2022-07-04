a1f7d61df3d450228236c386216098e2ffc8c245
iprankurpandey <pprankur@gmail.com>
2022-07-04 17:16:14 +0530

    updated styles and logic

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const btn = document.querySelector(".copy-btn");
+const btn1 = document.querySelector(".copy-btn");
 const code_url = document.querySelector(".code-url");
 const newCode_url = code_url.innerText;
 console.log(newCode_url);
 
 function changeBtnText() {
-  btn.innerHTML = "copied";
+  btn1.innerHTML = "copied";
   navigator.clipboard.writeText(newCode_url);
   /* Alert the copied text */
   alert("Copied the text: " + newCode_url);
 }
 
-btn.addEventListener("click", changeBtnText);
+btn1.addEventListener("click", changeBtnText);
